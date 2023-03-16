3641a2b282efbe6df8f1908b0db43abf36be2619
rodrickaheebwa <rodrickaheebwa@gmail.com>
2023-03-16 12:04:10 +0300

    cron

--- scheduled-scrapers/greeting.py
+++ scheduled-scrapers/greeting.py
@@ -5,3 +5,16 @@ today = date.today().strftime("%A %d %B %Y")
 
 with open(file_path, 'a') as f:
     f.write('\nGood morning Rodrick! Today is ' + today +  '\n')
+
+
+# ubuntu
+"""
+import datetime
+
+today_date = datetime.date.today().strftime("%A %d %B %Y")
+now_time = datetime.datetime.now().time().strftime("%I : %M %p")
+file_path = '/home/rodrick/Desktop/python-scheduling/greeting.txt'
+
+with open(file_path, 'a') as f:
+    f.write(f'Hello Rodrick!. The date is {today_date} and the time is {now_time}.\n')
+"""
